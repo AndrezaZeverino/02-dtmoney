6027c81396fa7872bb467d8d9c8f94e506e490ee
Andreza Zeverino <you@example.com>
2022-01-20 15:33:58 -0300

    styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,11 +2,21 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle `
 
+    :root{
+        --background: #f0f2f5;
+    }
+
     * {
         margin: 0;
         padding: 0;
+        box-sizing: border-box;
     }
 
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+     /* coloco acima deste modo para que todos os browsers fiquem com a letra "bonita"  */
+    }
 
 
 
