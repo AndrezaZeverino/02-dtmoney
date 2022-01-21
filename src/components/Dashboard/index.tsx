f111843bd495d056ec07deb0314d743731be8d3c
Andreza Zeverino <andrezazeverino@gmail.com>
2022-01-21 17:07:23 -0300

    finalização componentização, consumindo API

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,12 @@
 import { Container } from '../Dashboard/styles'
+import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
 
 export function Dashboard() {
     return(
         <Container>
-
+            <Summary/>
+            <TransactionsTable />
 
         </Container>
 
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,39 @@
+import  styled from 'styled-components';
+
+export const Container = styled.div `
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    /* Se coloco a propriedade acima, crio 3 colunas com tamanho flexível */
+    gap: 2rem;
+    margin-top: -10rem;
+    
+    div {
+        background: var(--shape);
+        padding: 1.5rem 2rem;
+        border-radius: 0.25rem;
+        color: var(--text-title);
+    
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    strong {
+        display: block;
+        /* coloco display block pois o strong não aceita margin */ 
+        margin-top: 1rem;
+        font-size: 2rem;
+        font-weight: 500;
+        line-height: 3rem;
+    }
+
+    &.highlight-background{
+        background: var(--green);
+        color: #fff;
+
+    }
+}
+
+`
