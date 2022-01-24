78766d9e2f1a46f2663a60c514c31fb3a251df3b
Andreza Zeverino <andrezazeverino@gmail.com>
2022-01-24 16:13:52 -0300

    Modal e Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setisNewTransactionModalOpen(true);
+      
+  }
+
+  function handleCloseNewTransactionModal() {
+      setisNewTransactionModalOpen(false);
+  /* o nome de função começa com handle informando que a pessoa precisa clicar algo, fazer algo, pra essa função executar */
+
+  }
+  
   return (
     <>
-        <Header />
+        <Header onOpenNewTransactionModal={
+          handleOpenNewTransactionModal
+        } />
         <Dashboard />
+
+        <NewTransactionModal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        />
+
         <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,22 @@
 import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import Modal from 'react-modal';
 
-export function Header() {
-    const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-    function handleOpenNewTransactionModal() {
-        
-    }
-
-
-
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return(
         <Container>
             <Content>
             {/* Container é o nome da minha função no css */}
             <img src={logoImg} alt="dt money"></img>
-            <button type="button">
+            <button type="button" onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
+
             </Content>
         </Container>
     )
